a36b78f35a82856242fbaf885b5c19031e0f1df8
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2024-04-04 20:27:48 +0000

    initial comit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://docs.passwordless.id',
+  integrations: [starlight({
+    title: 'Passwordless.ID Docs',
+    customCss: ['./src/styles/theme.css'],
+    logo: {
+      src: './src/assets/logo/logo-text.svg',
+      replacesTitle: true
+    },
+    social: {
+      github: 'https://github.com/passwordless-id'
+    },
+    sidebar: [{
+      label: 'Getting started',
+      autogenerate: {
+        directory: 'getting_started'
+      }
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), vue()]
+});
---
+++ src/components/MyCard.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: ['icon']
+}
+</script>
+
+<template>
+	<article class="card">
+		<img :src="icon">
+		<div>
+			<slot />
+		</div>
+	</article>
+</template>
+
+<style is:global>
+	.card {
+		display: flex;
+		align-items: center;
+		gap: 50px;
+		margin: 5rem 0;
+	}
+	.card > img {
+		height: 100px;
+		width: 100px;
+	}
+	.card > div {
+		/*text-align: left;*/
+	}
+
+	.card:nth-child(odd){
+		margin-left: 150px;
+		flex-direction: row-reverse;
+	}
+
+	.card:nth-child(even){
+		margin-right: 150px;
+		text-align: right;
+		
+	}
+</style>
